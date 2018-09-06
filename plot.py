f10101e59e0560710d38284a3f9fa66d16d38f8c
Laurent Courty <lrntct@gmail.com>
2018-09-06 19:00:03 +0100

    can now change map and legend titles

--- plot.py
+++ plot.py
@@ -13,7 +13,11 @@ import cartopy as ctpy
 import matplotlib.pyplot as plt
 
 DATA_DIR = '/home/lunet/gylc4/geodata/ERA5'
-ANNUAL_FILE_GUMBEL = 'era5_2000-2012_precip_gradient.nc'
+ANNUAL_FILE = 'era5_2000-2012_precip_gradient_ascorder.nc'
+PLOT_DIR = '../plot'
+
+EXTRACT = dict(latitude=slice(45, -45),
+               longitude=slice(0, 180))
 
 
 def plot_mean(da):
@@ -33,7 +37,7 @@ def plot_mean(da):
     plt.close()
 
 
-def gumbel_map(ds, var_names, fig_name, sqr=False):
+def gumbel_map(ds, var_names, disp_names, value_name, fig_name, sqr=False):
     crs = ctpy.crs.Robinson()
     sel = ds.loc[{'duration':24}]
     # reshape variables as dimension
@@ -46,23 +50,47 @@ def gumbel_map(ds, var_names, fig_name, sqr=False):
         da_sel.coords['param'] = [var_name]
         da_list.append(da_sel)
     da = xr.concat(da_list, 'param')
+    da.attrs['long_name'] = value_name  # Legend title
     p = da.plot(col='param', col_wrap=1,
                 transform=ctpy.crs.PlateCarree(),
                 aspect=ds.dims['longitude'] / ds.dims['latitude'],
                 cmap='viridis', robust=True, extend='both',
                 subplot_kws=dict(projection=crs)
                 )
-    for ax in p.axes.flat:
+    for ax, disp_name in zip(p.axes.flat, disp_names):
         ax.coastlines(linewidth=.5, color='black')
-    plt.savefig(fig_name)
+        ax.set_title(disp_name)
+    # plt.tight_layout()
+    plt.savefig(os.path.join(PLOT_DIR, fig_name))
+    plt.close()
+
+
+def da_map(da, fig_name):
+    crs = ctpy.crs.Robinson()
+    da.plot()
+            # transform=ctpy.crs.PlateCarree(),
+            # # aspect=da.sizes['longitude'] / da.sizes['latitude'],
+            # cmap='viridis', robust=True, extend='both',
+            # subplot_kws=dict(projection=crs)
+            # )
+    plt.savefig(os.path.join(PLOT_DIR, fig_name))
     plt.close()
 
 
 def main():
-    gumbel_coeff = xr.open_dataset(os.path.join(DATA_DIR, ANNUAL_FILE_GUMBEL))
-    gumbel_map(gumbel_coeff, ['loc_final', 'scale_final'], 'gumbel_params.png')
-    gumbel_map(gumbel_coeff, ['loc_lr_slope', 'scale_lr_slope'], 'gumbel_scaling.png')
-    gumbel_map(gumbel_coeff, ['prov_lr_rvalue', 'final_lr_rvalue', 'loc_lr_rvalue', 'scale_lr_rvalue'], 'gumbel_r2.png', sqr=True)
+    ds = xr.open_dataset(os.path.join(DATA_DIR, ANNUAL_FILE))
+    # print(ds[['scale_prov', 'scale_final']].loc[{'duration':24, 'latitude':0, 'longitude':slice(0, 1)}].load())
+    gumbel_map(ds, ['loc_final', 'scale_final'],
+                ['Location $\mu$', 'Scale $\sigma$'],
+                'Parameter value', 'gumbel_params.png')
+    gumbel_map(ds, ['loc_lr_slope', 'scale_lr_slope'],
+               ['Location-Duration relationship', 'Scale-duration relationship'],
+               '$\eta$', 'gumbel_scaling.png')
+    # print(ds.where(ds['log_location'].isnull()).count().compute())
+
+    gumbel_map(ds, ['prov_lr_rvalue', 'final_lr_rvalue', 'loc_lr_rvalue', 'scale_lr_rvalue'],
+               ['First Gumbel estimate', 'Final Gumbel fitting', 'Location-Duration', 'Scale-Duration'],
+               '$r^2$', 'gumbel_r2.png', sqr=True)
 
 
 
