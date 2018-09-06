0cb144b2d6cd29419cbfda968583a5268a49edb9
Laurent Courty <lrntct@gmail.com>
2018-09-06 17:19:56 +0100

    add a func to set attribute. Add a func to run linear regression with statsmodels

--- analysis.py
+++ analysis.py
@@ -13,14 +13,16 @@ from dask.diagnostics import ProgressBar
 import zarr
 import scipy.stats
 import dask_ml.linear_model
+import statsmodels as sm
+import statsmodels.regression.linear_model as slm
 
 
 DATA_DIR = '/home/lunet/gylc4/geodata/ERA5'
 HOURLY_FILE = 'era5_2000-2012_precip_big_chunks.zarr'
 ANNUAL_FILE = 'era5_2000-2012_precip_annual_max.zarr'
 ANNUAL_FILE_RANK = 'era5_2000-2012_precip_ranked.zarr'
-ANNUAL_FILE_GUMBEL = 'era5_2000-2012_precip_gumbel.nc'
-ANNUAL_FILE_GRADIENT = 'era5_2000-2012_precip_gradient.nc'
+ANNUAL_FILE_GUMBEL = 'era5_2000-2012_precip_gumbel_ascorder.nc'
+ANNUAL_FILE_GRADIENT = 'era5_2000-2012_precip_gradient.zarr'
 
 LOG_FILENAME = 'Analysis_log_{}.csv'.format(str(datetime.now()))
 
@@ -35,8 +37,8 @@ KISUMU_COORD = (0.1, 34.75)
 # Extract
 # EXTRACT = dict(latitude=slice(1.0, -0.25),
 #                longitude=slice(32.5, 35))
-EXTRACT = dict(latitude=slice(0, -5),
-               longitude=slice(0, 5))
+EXTRACT = dict(latitude=slice(0, -4),
+               longitude=slice(0, 4))
 # Spatial resolution in degree - used for match coordinates
 SPATIAL_RES = 0.25
 
@@ -48,7 +50,8 @@ DTYPE = 'float32'
 
 HOURLY_CHUNKS = {'time': -1, 'latitude': 8, 'longitude': 8}
 ANNUAL_CHUNKS = {'year': -1, 'duration':1, 'latitude': 45*4, 'longitude': 45*4}  # 4 cells: 1 degree
-ANNUAL_ENCODING = {'annual_max': {'dtype': DTYPE, 'compressor': zarr.Blosc(cname='lz4', clevel=9)},
+GEN_FLOAT_ENCODING = {'dtype': DTYPE, 'compressor': zarr.Blosc(cname='lz4', clevel=9)}
+ANNUAL_ENCODING = {'annual_max': GEN_FLOAT_ENCODING,
                    'duration': {'dtype': DTYPE},
                    'latitude': {'dtype': DTYPE},
                    'longitude': {'dtype': DTYPE}}
@@ -76,6 +79,16 @@ def step1_annual_maxs_of_roll_mean(ds, durations, temp_res):
     return xr.concat(annual_maxs, 'duration')
 
 
+def slm_ols_wrapper(x, y):
+    """call statsmodels OLS and make it look like scipy linregress
+    ['slope', 'intercept', 'rsquared', 'pvalue', 'stderr']
+    """
+    X = sm.tools.tools.add_constant(x)
+    ols = slm.OLS(y, X, missing='drop')
+    results = ols.fit()
+    return results.params[1], results.params[0], results.rsquared, results.pvalues[0], results.bse[0]
+
+
 def linregress(ds, x, y, prefix, dims):
     """ds: xarray dataset
     x, y: name of variables to use for the regression
@@ -85,6 +98,7 @@ def linregress(ds, x, y, prefix, dims):
     lr_params = ['slope', 'intercept', 'rvalue', 'pvalue', 'stderr']
     array_names = ['{}_{}'.format(prefix, n) for n in lr_params]
 
+    # linregress faster without dask
     ds.load()
     # return a tuple of DataArrays
     res = xr.apply_ufunc(scipy.stats.linregress, ds[x], ds[y],
@@ -159,6 +173,15 @@ def step3_duration_gradient(ds):
     linregress(ds, 'log_duration', 'log_scale', 'scale_lr', ['duration'])
 
 
+def set_attrs(ds):
+    """Set atrributes of a dataset
+    """
+    # set the originating_centre as an atribute, not a coordinate
+    ds.attrs['originating_centre'] = str(ds.coords['originating_centre'].values)
+    ds = ds.drop('originating_centre')
+    return ds
+
+
 def benchmark(ds):
     """Run the gumbel fit for a number of extract sizes
     print result to stdout
@@ -214,7 +237,7 @@ def main():
                        #'duration': 6
                        }
     # Log file
-    logger(['operation', 'timestamp', 'cumul_sec'])
+    # logger(['operation', 'timestamp', 'cumul_sec'])
 
     with ProgressBar():
         start_time = datetime.now()
@@ -235,31 +258,37 @@ def main():
         # annual_maxs = amax_rob()  # to compare with Rob's values
 
         # Do the ranking
-        # ds_ranked = step21_ranking(annual_maxs)#.chunk(ANNUAL_CHUNKS)
+        # ds_ranked = step21_ranking(annual_maxs).chunk(ANNUAL_CHUNKS)
         # print(ds_ranked)
         # logger(['start writing ranks', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
-        encoding = copy.deepcopy(ANNUAL_ENCODING)
-        encoding['rank'] = {'dtype': 'float32', 'compressor': zarr.Blosc(cname='lz4', clevel=9)}
+        # encoding = copy.deepcopy(ANNUAL_ENCODING)
+        # encoding['rank'] = GEN_FLOAT_ENCODING
         rank_path = os.path.join(DATA_DIR, ANNUAL_FILE_RANK)
         # ds_ranked.to_zarr(rank_path, mode='w', encoding=encoding)
 
 
         # fit Gumbel #
-        logger(['start gumbel fitting', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
-        ds_ranked = xr.open_zarr(rank_path)
-        ds_fitted = step22_gumbel_fit(ds_ranked)
+        # logger(['start gumbel fitting', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
+        # ds_ranked = set_attrs(xr.open_zarr(rank_path)).loc[EXTRACT]
+        # ds_fitted = step22_gumbel_fit(ds_ranked)
         # logger(['start writting results of gumbel fitting', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
-        # gumbel_path = os.path.join(DATA_DIR, ANNUAL_FILE_GUMBEL)
-        # ds_fitted.to_netcdf(gumbel_path, mode='w')
-        # ds_fitted = xr.open_dataset(gumbel_path)
+        gumbel_path = os.path.join(DATA_DIR, ANNUAL_FILE_GUMBEL)
+        # encoding = {v:GEN_FLOAT_ENCODING for v in ds_fitted.data_vars.keys()}
+        # ds_fitted.chunk(ANNUAL_CHUNKS).to_zarr(gumbel_path, mode='w', encoding=encoding)
+        ds_fitted = set_attrs(xr.open_dataset(gumbel_path))
+        print(ds_fitted)
 
         # fit duration scaling #
+        # np.seterr(all='raise')
+
         # logger(['start duration scaling fitting', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
-        # step3_duration_gradient(ds_fitted)
+        # step3_duration_gradient(ds_fitted).log[EXTRACT]
         # logger(['start writing duration scaling', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
-        # gradient_path = os.path.join(DATA_DIR, ANNUAL_FILE_GRADIENT)
-        # ds_fitted.chunk(ANNUAL_CHUNKS).to_netcdf(gradient_path, mode='w')
+        gradient_path = os.path.join(DATA_DIR, ANNUAL_FILE_GRADIENT)
+        encoding = {v:GEN_FLOAT_ENCODING for v in ds_fitted.data_vars.keys()}
+        # ds_fitted.chunk(ANNUAL_CHUNKS).to_zarr(gradient_path, mode='w', encoding=encoding)
         # logger(['complete', str(datetime.now()), (datetime.now()-start_time).total_seconds()])
+        # print(ds_fitted.isnull().sum().compute())
 
         # display params to compare with Rob's
         # print(ds_fitted[['loc_final', 'scale_final']].loc[{'site':'kampala'}].to_dataframe().transpose())
