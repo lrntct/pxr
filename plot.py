3df5e0a4e263c391c9b0e3aafb81d34282ca8e63
Laurent Courty <lrntct@gmail.com>
2018-09-12 16:26:50 +0100

    add plot of gumbel CDF

--- plot.py
+++ plot.py
@@ -15,7 +15,8 @@ import matplotlib.pyplot as plt
 
 DATA_DIR = '/home/lunet/gylc4/geodata/ERA5'
 HOURLY_FILE = 'era5_2000-2012_precip.zarr'
-ANNUAL_FILE = 'era5_2000-2012_precip_pearsonr.zarr'
+ANNUAL_FILE = 'era5_2000-2012_precip_scaling.zarr'
+GAUGES_FILE = '../data/GHCN/ghcn.nc'
 PLOT_DIR = '../plot'
 
 EXTRACT = dict(latitude=slice(45, -45),
@@ -40,7 +41,7 @@ def single_map(da, cbar_label, title, fig_name):
     plt.close()
 
 
-def gumbel_map(ds, var_names, disp_names, value_name, fig_name, sqr=False):
+def multi_maps(ds, var_names, disp_names, value_name, fig_name, sqr=False):
     crs = ctpy.crs.Robinson()
     sel = ds.loc[{'duration':24}]
     # reshape variables as dimension
@@ -69,15 +70,17 @@ def gumbel_map(ds, var_names, disp_names, value_name, fig_name, sqr=False):
     plt.close()
 
 
-def plot_sites(ds, sites, fig_name):
+def scaling_per_site(ds, sites, fig_name):
     # extract sites values from the dataset as pandas dataframes
     dict_df = {}
     scaling_coeffs_series = []
     for site_name, site_coord in sites.items():
         ds_sel = ds.sel(latitude=site_coord[0], longitude=site_coord[1], method='nearest').drop(['latitude', 'longitude'])
-        scaling_coeffs = ['loc_lr_intercept', 'loc_lr_slope', 'scale_lr_intercept', 'scale_lr_slope', 'scaling_pearsonr']
+        scaling_coeffs = ['loc_lr_intercept', 'loc_lr_slope',
+                          'scale_lr_intercept', 'scale_lr_slope',
+                          'scaling_pearsonr', 'scaling_spearmanr', 'scaling_ratio']
         scaling_coeffs_series.append(ds_sel[scaling_coeffs].to_array(name=site_name).to_series())
-        dict_df[site_name] = ds_sel[['loc_final', 'scale_final']].to_dataframe()
+        dict_df[site_name] = ds_sel[['loc_loaiciga', 'scale_loaiciga']].to_dataframe()
     df_scaling_coeffs = pd.DataFrame(scaling_coeffs_series).T
     # print(df_scaling_coeffs)
     # print(dict_df['Kisumu'])
@@ -95,9 +98,9 @@ def plot_sites(ds, sites, fig_name):
             regline_col = '{}_lr'.format(p)
             df[regline_col] = 10**intercept * df.index**slope
         # plot
-        linesyles = {'loc_final': dict(linestyle='None', marker='o', color='#1b9e77', label='Location $\mu$'),
+        linesyles = {'loc_loaiciga': dict(linestyle='None', marker='o', color='#1b9e77', label='Location $\mu$'),
                      'loc_lr': dict(linestyle='solid', marker=None, color='#1b9e77', label='$d^{\eta(\mu)}$'),
-                     'scale_final': dict(linestyle='None', marker='o', color='#d95f02', label='Scale $\sigma$'),
+                     'scale_loaiciga': dict(linestyle='None', marker='o', color='#d95f02', label='Scale $\sigma$'),
                      'scale_lr': dict(linestyle='solid', marker=None, color='#d95f02', label='$d^{\eta(\sigma)}$'),
                     }
         for col, styles in linesyles.items():
@@ -110,11 +113,19 @@ def plot_sites(ds, sites, fig_name):
         ax.set_xticks([1, 3, 6, 12, 24, 48, 120, 360])
         ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
-        txt = "$\eta(\mu)$ = {:.2f}\n$\eta(\sigma)$ = {:.2f}\nPearson's r = {:.2f}".format(
-                eta['loc'], eta['scale'],
-                df_scaling_coeffs.loc['scaling_pearsonr', site_name])
-        ax.text(0.05, 0.15, txt, horizontalalignment='left', backgroundcolor='white',
+        txt = ("$\eta(\mu)$ = {:.2f}\n"
+               "$\eta(\sigma)$ = {:.2f}\n"
+               "Pearson's r = {:.2f}\n"
+               "Spearman's $\\rho$ = {:.2f}\n"
+               "$\eta(\mu) / \eta(\sigma)$  = {:.2f}").format(
+                        eta['loc'], eta['scale'],
+                        df_scaling_coeffs.loc['scaling_pearsonr', site_name],
+                        df_scaling_coeffs.loc['scaling_spearmanr', site_name],
+                        df_scaling_coeffs.loc['scaling_ratio', site_name]
+                        )
+        t = ax.text(0.05, 0.20, txt, horizontalalignment='left', backgroundcolor='white',
                 verticalalignment='center', transform=ax.transAxes, size=10)
+        t.set_bbox(dict(alpha=0))
     # plt.legend(lines, labels, loc='lower center', ncol=4)
     lgd = fig.legend(lines, labels, loc='lower center', ncol=4)
     plt.tight_layout()
@@ -137,28 +148,88 @@ def plot_hourly(ds, sites, fig_name):
     plt.close()
 
 
+def plot_gumbel_per_site(ds, sites, fig_name):
+    DURATION = 6
+    # extract sites values from the dataset as pandas dataframes
+    dict_cdf = {}
+    # dict_gof = {}
+    for site_name, site_coord in sites.items():
+        dict_pvalues = {}
+        ds_sel = ds.sel(latitude=site_coord[0],
+                        longitude=site_coord[1],
+                        duration=DURATION,
+                        method='nearest').drop(['latitude', 'longitude', 'duration'])
+        # Keep only wanted values as dataframe
+        # print(ds_sel)
+        keep_col = ['estim_prob', 'analytic_prob_moments', 'analytic_prob_loaiciga', 'annual_max']
+        # print(ds_sel.load())
+        df_cdf = ds_sel[keep_col].load().to_dataframe().set_index('annual_max', drop=True).sort_index()
+        # print(df_cdf)
+        dict_cdf[site_name] = df_cdf
+        dict_gof[site_name] = pd.Series(dict_pvalues)
+
+    # df_gof = pd.DataFrame(dict_gof)
+    # df_gof.to_csv('GoF.csv')
+
+    fig, axes = plt.subplots(1, 2, sharey=True, figsize=(8,4))
+    for (site_name, df), ax in zip(dict_cdf.items(), axes):
+        dict_cdf[site_name].plot(linestyle=None, marker='o', markersize=1.5,
+                                 title=site_name, ax=ax, legend=False)
+        lines, labels = ax.get_legend_handles_labels()
+        ax.set_ylabel('Cumulative probability')
+        txt = "Dcrit = {:.2f}".format(ds['Dcrit_5pct'].values)
+        ax.text(0.65, 0.10, txt, horizontalalignment='left', backgroundcolor='white',
+                verticalalignment='center', transform=ax.transAxes, size=10)
+    fig.suptitle('Cumulative probability for a duration of {} hours'.format(DURATION))
+    lgd_labels = ['Estimated CDF',
+                  'Analytic CDF (method of moments)',
+                  'Analytic CDF (iterative method)']
+    lgd = fig.legend(lines, lgd_labels, loc='lower center', ncol=4)
+    # plt.tight_layout()
+    plt.subplots_adjust(bottom=.24, wspace=None, hspace=None)
+    plt.savefig(os.path.join(PLOT_DIR, fig_name))
+    plt.close()
+
+
 def main():
-    ds = xr.open_zarr(os.path.join(DATA_DIR, ANNUAL_FILE))
+    ds_era = xr.open_zarr(os.path.join(DATA_DIR, ANNUAL_FILE))
+    print(ds_era)
 
     # print(ds[['scale_prov', 'scale_final']].loc[{'duration':24, 'latitude':0, 'longitude':slice(0, 1)}].load())
-    # gumbel_map(ds, ['loc_final', 'scale_final'],
+    # multi_maps(ds_era, ['loc_final', 'scale_final'],
     #             ['Location $\mu$', 'Scale $\sigma$'],
     #             'Parameter value', 'gumbel_params.png')
-    # gumbel_map(ds, ['loc_lr_slope', 'scale_lr_slope'],
+    # multi_maps(ds_era, ['loc_lr_slope', 'scale_lr_slope'],
     #            ['Location-Duration relationship', 'Scale-duration relationship'],
     #            '$\eta$', 'gumbel_scaling.png')
-    # print(ds.where(ds['log_location'].isnull()).count().compute())
+    # print(ds.where(ds_era['log_location'].isnull()).count().compute())
 
-    # gumbel_map(ds, ['prov_lr_rvalue', 'final_lr_rvalue', 'loc_lr_rvalue', 'scale_lr_rvalue'],
+    # multi_maps(ds_era, ['prov_lr_rvalue', 'final_lr_rvalue', 'loc_lr_rvalue', 'scale_lr_rvalue'],
     #            ['First Gumbel estimate', 'Final Gumbel fitting', 'Location-Duration', 'Scale-Duration'],
     #            '$r^2$', 'gumbel_r2.png', sqr=True)
 
-    plot_sites(ds, STUDY_SITES, 'sites_scaling.png')
+    # print((~np.isfinite(ds)).sum().compute())
+    # plot_gumbel_per_site(ds, STUDY_SITES, 'sites_gumbel.png')
+    # scaling_per_site(ds_era, STUDY_SITES, 'sites_scaling.png')
 
-    # single_map(ds['scaling_pearsonr'],
+    # single_map(ds_era['scaling_pearsonr'],
     #            title="$d^{\eta(\mu)}$ - $d^{\eta(\sigma)}$ correlation",
     #            cbar_label='Pearson correlation coefficient',
-    #            fig_name='pearsonr.pdf')
+    #            fig_name='pearsonr.png')
+
+    # single_map(ds_era['scaling_ratio'],
+    #            title="Parameter scaling ratio",
+    #            cbar_label='$\eta(\mu) / \eta(\sigma)$',
+    #            fig_name='scaling_ratio.png')
+
+    # single_map(ds_era['scaling_spearmanr'],
+    #        title="Parameter scaling correlation",
+    #        cbar_label="Spearman's $\\rho$",
+    #        fig_name='spearmanr.png')
+
+    # multi_maps(ds_era.loc[{'duration':6}], ['ks_loaiciga', 'ks_moments'],
+    #            ['Fitting accuracy for iterative method (d=6h)', 'Fitting accuracy for mthod of moments (d=6h)'],
+    #            "Kolmogorov-Smirnov's D", 'fitting_accuracy.png', sqr=False)
 
     # hourly_path = os.path.join(DATA_DIR, HOURLY_FILE)
     # hourly = xr.open_zarr(hourly_path)
