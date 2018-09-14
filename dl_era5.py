3fdc1ff78b332aa92a2e3f6eb8f0b3895de557a2
Laurent Courty <lrntct@gmail.com>
2018-09-14 12:17:39 +0100

    DL months in parallel. DL total precip

--- dl_era5.py
+++ dl_era5.py
@@ -10,7 +10,8 @@ import cdsapi
 
 FORMAT = 'grib'
 PRODUCT = 'reanalysis-era5-single-levels'
-VARIABLE = 'mean_total_precipitation_rate'
+VARIABLE = [#'mean_total_precipitation_rate',
+            'total_precipitation']
 TYPE = 'reanalysis'
 MONTH = ['01','02','03','04','05','06','07','08','09','10','11','12']
 DAY = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15',
@@ -19,23 +20,25 @@ TIME = ['00:00','01:00','02:00','03:00','04:00','05:00',
         '06:00','07:00','08:00','09:00','10:00','11:00',
         '12:00','13:00','14:00','15:00','16:00','17:00',
         '18:00','19:00','20:00','21:00','22:00','23:00']
-START_YEAR = 2000
-END_YEAR = 2017
+START_YEAR = 2015
+END_YEAR = 2015
 
 
-def dl_cdsapi(year):
+def dl_cdsapi(year_month):
     c = cdsapi.Client()
-    for month in MONTH:
-        query = {'variable':VARIABLE,
-                 'product_type':TYPE,
-                 'year':str(year),
-                 'month':month,
-                 'day': DAY,
-                 'time':TIME,
-                 'format':FORMAT}
-        r = c.retrieve(PRODUCT, query)
-        filename = '{}-{}.{}'.format(year, month, FORMAT)
-        r.download(filename)
+    # try:
+    query = {'variable': VARIABLE,
+             'product_type': TYPE,
+             'year': str(year_month[0]),
+             'month': year_month[1],
+             'day': DAY,
+             'time': TIME,
+             'format':FORMAT}
+    r = c.retrieve(PRODUCT, query)
+    filename = '{}-{}.{}'.format(year_month[0], year_month[1], FORMAT)
+    r.download(filename)
+    # except:
+    #     pass
 
 
 # def dl_ecmwf(year):
@@ -60,7 +63,9 @@ def dl_cdsapi(year):
 def main():
     pool = mp.Pool(10)
     years = range(START_YEAR, END_YEAR+1)
-    pool.map(dl_cdsapi, years)
+    year_month = [(y, m) for y in years for m in MONTH]
+    # print(year_month)
+    pool.map(dl_cdsapi, year_month)
 
 if __name__ == "__main__":
     sys.exit(main())
