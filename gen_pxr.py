808e78a78eff22650b48d70a5f09e1f996b495cd
Laurent Courty <lrntct@gmail.com>
2018-12-07 00:56:59 +0000

    add code to generate PXR4

--- gen_pxr.py
+++ gen_pxr.py
@@ -26,22 +26,58 @@ def gen_pxr2(ds_source):
     ds_pxr2.attrs.update(metadata['pxr2'])
 
     for coordinate in ds_pxr2.coords:
-        ds_pxr2.coords[coordinate].attrs.update(metadata['coords'][coordinate])
+        try:
+            ds_pxr2.coords[coordinate].attrs.update(metadata['coords'][coordinate])
+        except KeyError:
+            pass
     for var in ds_pxr2.data_vars:
         try:
             ds_pxr2.data_vars[var].attrs.update(metadata['data_vars'][var])
         except KeyError:
             pass
-        print(ds_pxr2.data_vars[var])
-    print(ds_pxr2)
     return ds_pxr2
+
+
+def gen_pxr4(ds_source):
+    # Select the relevant variables
+    ds_pxr4 = ds_source[['location_line_intercept', 'location_line_slope', 'location_line_rvalue',
+                         'scale_line_intercept', 'scale_line_slope', 'scale_line_rvalue']]
+    ds_pxr4['a'] = 10**ds_pxr4['location_line_intercept']
+    ds_pxr4['b'] = 10**ds_pxr4['scale_line_intercept']
+    ds_pxr4['location_r2'] = ds_pxr4['location_line_rvalue'] * ds_pxr4['location_line_rvalue']
+    ds_pxr4['scale_r2'] = ds_pxr4['scale_line_rvalue'] * ds_pxr4['scale_line_rvalue']
+    for d in ['location_line_intercept', 'scale_line_intercept',
+              'location_line_rvalue', 'scale_line_rvalue']:
+        del ds_pxr4[d]
+    rename_dict = {'location_line_slope': 'alpha',
+                   'scale_line_slope': 'beta'}
+    ds_pxr4.rename(rename_dict, inplace=True)
+    # Dataset description
+    ds_pxr4.attrs.update(metadata['common'])
+    ds_pxr4.attrs.update(metadata['pxr4'])
+
+    for coordinate in ds_pxr4.coords:
+        try:
+            ds_pxr4.coords[coordinate].attrs.update(metadata['coords'][coordinate])
+        except KeyError:
+            pass
+    for var in ds_pxr4.data_vars:
+        try:
+            ds_pxr4.data_vars[var].attrs.update(metadata['data_vars'][var])
+        except KeyError:
+            pass
+    return ds_pxr4
 
 
 def main():
     source_path = os.path.join(DATA_DIR, SOURCE)
-    ds_source = xr.open_zarr(source_path).sel(gumbel_fit=b'scipy', drop=True)
+    ds_source = xr.open_zarr(source_path).sel(gumbel_fit=b'scipy', scaling_extent=b'all', drop=True)
+    print(ds_source)
     ds_pxr2 = gen_pxr2(ds_source)
     # ds_pxr2.to_netcdf(os.path.join(DATA_DIR, PXR2))
+    ds_pxr4 = gen_pxr4(ds_source)
+    print(ds_pxr4)
+    ds_pxr4.to_netcdf(os.path.join(DATA_DIR, PXR4))
 
 
 if __name__ == "__main__":
