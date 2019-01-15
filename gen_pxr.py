0258059db19dd868040177ef32295ce047aea0f2
Laurent Courty <lrntct@gmail.com>
2019-01-15 16:29:26 +0000

    change authors order. Add ratios

--- gen_pxr.py
+++ gen_pxr.py
@@ -35,7 +35,7 @@ def apply_common_metadata(ds):
 
 def gen_pxr2(ds_source):
     # Select the relevant variables
-    ds_pxr2 = ds_source[['location', 'scale', 'A2', 'A2_crit']]
+    ds_pxr2 = ds_source.sel(scaling_extent='all', drop=True)[['location', 'scale', 'A2', 'A2_crit']]
     # Metadata
     apply_common_metadata(ds_pxr2)
     ds_pxr2.attrs.update(metadata['pxr2'])
@@ -56,6 +56,11 @@ def gen_pxr4(ds_source):
     rename_dict = {'location_line_slope': 'alpha',
                    'scale_line_slope': 'beta'}
     ds_pxr4.rename(rename_dict, inplace=True)
+    # Compute ratio
+    ds_all = ds_source.sel(scaling_extent='all')
+    ds_daily =  ds_source.sel(scaling_extent='daily')
+    ds_pxr4['alpha_ratio'] = ds_all['location_line_slope'] / ds_daily['location_line_slope']
+    ds_pxr4['beta_ratio'] = ds_all['scale_line_slope'] / ds_daily['scale_line_slope']
     # Metadata
     apply_common_metadata(ds_pxr4)
     ds_pxr4.attrs.update(metadata['pxr4'])
@@ -63,14 +68,15 @@ def gen_pxr4(ds_source):
 
 
 def write_datasets(ds_source):
+    # print(ds_source)
     ds_pxr2 = gen_pxr2(ds_source)
     ds_pxr2.to_netcdf(os.path.join(DATA_DIR, PXR2))
     ds_pxr4 = gen_pxr4(ds_source)
+    # print(ds_pxr4)
     ds_pxr4.to_netcdf(os.path.join(DATA_DIR, PXR4))
 
 
 def test_datasets():
-    pxr2 = xr.open_dataset(os.path.join(DATA_DIR, PXR2))
     for file_name in [PXR2, PXR4]:
         ds = xr.open_dataset(os.path.join(DATA_DIR, file_name))
         print(ds)
@@ -82,11 +88,10 @@ def test_datasets():
 
 def main():
     source_path = os.path.join(DATA_DIR, SOURCE)
-    ds_source = xr.open_zarr(source_path).sel(gumbel_fit=b'scipy', scaling_extent=b'all', drop=True)
+    ds_source = xr.open_zarr(source_path).sel(gumbel_fit=b'scipy', drop=True).sel(scaling_extent=[b'all', b'daily'])
+    ds_source['scaling_extent'] = ds_source['scaling_extent'].astype(str)
     write_datasets(ds_source)
     test_datasets()
-
-
 
 
 if __name__ == "__main__":
