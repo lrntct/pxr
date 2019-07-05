d44e0f75151d6f73b6a53a4801ab26a3e5ac4627
Laurent Courty <lrntct@gmail.com>
2019-07-04 19:46:19 -0500

    adapt site plot for paper

--- plot.py
+++ plot.py
@@ -230,7 +230,7 @@ def plot_scaling_per_site(ds, fig_name):
     col_num = 2
     row_num = math.ceil(len(dict_df) / col_num)
     # fig_size = (9, 5)  # landscape
-    fig_size = (5, 8)  # portrait
+    fig_size = (6, 8)  # portrait
     fig = plt.figure(figsize=fig_size)
     ax_num = 1
 
